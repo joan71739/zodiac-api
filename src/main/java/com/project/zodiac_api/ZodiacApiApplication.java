2c2c3ec77aaf64ce96d0fcd591a77916791ab201
joan71739 <joan71739@gmail.com>
2026-05-09 21:43:46 +0800

    Initial commit

---
+++ src/main/java/com/project/zodiac_api/ZodiacApiApplication.java
@@ -0,0 +1,13 @@
+package com.project.zodiac_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZodiacApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZodiacApiApplication.class, args);
+	}
+
+}
